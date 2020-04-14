1be0428f058874a3e164c790d3942516bce671f0
dfuenteslepe <dfuentes@tricot.cl>
2020-04-14 09:31:36 -0400

    14.04.2020 Subida Controller Log
    subida de archivos LogAudio, LogDetalleEvaluacion,LogEvaluacionParcial

--- controller/LogAudioController.php
+++ controller/LogAudioController.php
@@ -1,11 +1,13 @@
 <?php
+
+    include(dirModel."LogAudio.php");
 	class LogAudioController {
 
 		private $databaseTransaction;
 
 		//constructor del controlador de Area
-		public function __construct($databaseTransaction) {
-			$this->databaseTransaction = $databaseTransaction;
+		public function __construct() {
+			$this->databaseTransaction = new DatabaseTransaction();
 		}
 
 		//devuelve el objeto inicializado por el controlador de DatabaseTransaction (Conexion contra la base de datos)
@@ -27,11 +29,12 @@
 		//funcion retorna un arreglo de todos los registros que encuentre en la tabla
 		public function listar() {
 			try {
-				$consulta = 'SELECT * FROM log_audio';
+				$consulta = 'SELECT * FROM log_audio ORDER BY fecha DESC';
 				//ejecutando la consulta
 				if($this->databaseTransaction != null) {
 					$resultado = $this->databaseTransaction->ejecutar($consulta);
 					if($this->databaseTransaction->cantidadResultados() == 0) {
+						$this->databaseTransaction->cerrar();
 						return null;
 					}else{
 						$array = null;
@@ -40,6 +43,7 @@
 							$array[$i] = new LogAudio($registro);
 							$i++;
 						}
+						$this->databaseTransaction->cerrar();
 						return $array;
 					}
 				}else{
@@ -54,18 +58,60 @@
 
 		public function listarPorCodigo($codigo) {
 			try {
-				$consulta = "SELECT * FROM log_audio WHERE codigo = '".$codigo."'";
+				$consulta = "SELECT * FROM log_audio WHERE id = '".$codigo."'";
 				//ejecutando la consulta
 				if($this->databaseTransaction != null) {
 					$resultado = $this->databaseTransaction->ejecutar($consulta);
-					if($this->databaseTransaction->cantidadResultados() == 1) {
-						return new LogAudio($this->databaseTransaction->resultados());
-					}else{
-						if(ambiente == 'DEV') { echo "LogAudioController - listarPorCodigo: La consulta SQL devolvió mas de un resultado por eso se devuelve NULL"; }
+					if($this->databaseTransaction->cantidadResultados() == 0) {
+						$this->databaseTransaction->cerrar();
 						return null;
+					}else{
+						$array = null;
+						$i 	   = 0;
+						while($registro = $this->databaseTransaction->resultados()) {
+							$array[$i] = new LogAudio($registro);
+							$i++;
+						}
+						$this->databaseTransaction->cerrar();
+						return $array;
 					}
 				}else{
 					if(ambiente == 'DEV') { echo "LogAudioController - listarPorCodigo: El objeto DatabaseTransaction se encuentra nulo"; }
+					return false;
+				}
+			}catch(Exception $e) {
+				if(ambiente == 'DEV') { echo $e->getMessage(); }
+				return false;
+			}
+		}
+
+		public function ingresar($LogAudio) {
+			try {
+				//objeto
+				$obj = $Adjunto;
+				if($obj != null) {
+					//construyendo string
+					$consulta = "INSERT INTO log_audio ";
+					$consulta = $consulta."(fecha, usuario, numero_evaluacion, fecha_carga, nombre_audio,periodo) VALUES ";
+					$consulta = $consulta."('".date('Y-m-d H:i:s')."','".$obj->getusuario()."', ".$obj->getnumero_evaluacion().", '".$obj->getfecha_carga()."', '".$obj->getnombre_audio()."', '".$obj->getperiodo()."');";
+					//ejecutando la consulta
+					if($this->databaseTransaction != null) {
+						$resultado = $this->databaseTransaction->ejecutar($consulta);
+						if($resultados == true) {
+							$this->databaseTransaction->confirmar();
+							$this->databaseTransaction->cerrar();
+							return 1;
+						}else{
+							$this->databaseTransaction->deshacer();
+							$this->databaseTransaction->cerrar();
+							return 0;
+						}
+					}else{
+						if(ambiente == 'DEV') { echo "LogAudioController - ingresar: El objeto DatabaseTransaction se encuentra nulo"; }
+						return false;
+					}
+				}else{
+					if(ambiente == 'DEV') { echo "LogAudioController - ingresar: El objeto Adjunto (Model) se encuentra nulo"; }
 					return false;
 				}
 			}catch(Exception $e) {
